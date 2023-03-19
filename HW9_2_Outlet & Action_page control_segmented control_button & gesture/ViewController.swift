7c41bbbd3d94dfb9a32ce3f92b17802acd07a0f9
Daweihao84 <dwsamurai84@gmail.com>
2023-03-19 21:40:34 +0800

    Initial Commit

---
+++ HW9_2_Outlet & Action_page control_segmented control_button & gesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW9_2_Outlet & Action_page control_segmented control_button & gesture
+//
+//  Created by Dawei Hao on 2023/3/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
